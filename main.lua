e941f6ca6e83b5fedb999fa927355c777a184465
Mystikfluu <50167590+Mystikfluu@users.noreply.github.com>
2020-08-06 05:18:44 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -28,16 +28,13 @@ while wait(0.1) and _G.togglecraft do
 end
 end)
 end)
-Pets:Label("Auto delete is if below both configurable values, Auto delete2 is if below one Value.")
-warn("Auto delete is if below both configurable values, Auto delete2 is if below one Value.")
-print("Auto delete deletes Pets which have their Multipliers below the point of the configurable values called 'DeleteClicks' and 'DeleteDiamonds'")
-Pets:Toggle("Auto delete", function(toggle)
+Pets:Toggle("delete common", function(toggle)
+_G.common = toggle
 spawn(function()
-_G.delete = toggle
-while wait(0.1) and _G.delete do
+while wait(0.1) and _G.common do
 for i, v in pairs(game.Players.LocalPlayer.Pets:GetChildren()) do
   wait()
-  if(tonumber(v.DiamondMultiplier.Value) <= tonumber(_G.deletedm)) and (tonumber(v.ClickMultiplier.Value) <= tonumber(_G.deletecm)) then
+  if(game.ReplicatedStorage.PetLibrary.Pets:FindFirstChild(v.Name).Settings.Rarity.Value == "Common") then
     local args = {
       [1] = v.Pet_ID.Value,
       [2] = true
@@ -48,13 +45,13 @@ end
 end
 end)
 end)
-Pets:Toggle("Auto delete2", function(toggle)
+Pets:Toggle("delete rare", function(toggle)
+_G.rare = toggle
 spawn(function()
-_G.delete = toggle
-while wait(0.1) and _G.delete do
+while wait(0.1) and _G.rare do
 for i, v in pairs(game.Players.LocalPlayer.Pets:GetChildren()) do
 wait()
-if(tonumber(v.DiamondMultiplier.Value) <= tonumber(_G.deletedm)) or (tonumber(v.ClickMultiplier.Value) <= tonumber(_G.deletecm)) then
+if(game.ReplicatedStorage.PetLibrary.Pets:FindFirstChild(v.Name).Settings.Rarity.Value == "Rare") then
   local args = {
     [1] = v.Pet_ID.Value,
     [2] = true
@@ -64,6 +61,77 @@ end
 end
 end
 end)
+end)
+Pets:Toggle("delete epic", function(toggle)
+_G.epic = toggle
+spawn(function()
+while wait(0.1) and _G.epic do
+for i, v in pairs(game.Players.LocalPlayer.Pets:GetChildren()) do
+wait()
+if(game.ReplicatedStorage.PetLibrary.Pets:FindFirstChild(v.Name).Settings.Rarity.Value == "Epic") then
+local args = {
+  [1] = v.Pet_ID.Value,
+  [2] = true
+}
+game:GetService("ReplicatedStorage").RemoteEvents.PetDelete:FireServer(unpack(args))
+end
+end
+end
+end)
+end)
+Pets:Toggle("delete Legendary", function(toggle)
+_G.Legendary = toggle
+spawn(function()
+while wait(0.1) and _G.Legendary do
+for i, v in pairs(game.Players.LocalPlayer.Pets:GetChildren()) do
+wait()
+if(game.ReplicatedStorage.PetLibrary.Pets:FindFirstChild(v.Name).Settings.Rarity.Value == "Legendary") then
+local args = {
+[1] = v.Pet_ID.Value,
+[2] = true
+}
+game:GetService("ReplicatedStorage").RemoteEvents.PetDelete:FireServer(unpack(args))
+end
+end
+end
+end)
+end)
+warn("Auto delete is if below both configurable values, Auto delete2 is if below one Value.")
+print("Auto delete deletes Pets which have their Multipliers below the point of the configurable values called 'DeleteClicks' and 'DeleteDiamonds'")
+Pets:Toggle("(OLD) Auto delete", function(toggle)
+spawn(function()
+_G.delete = toggle
+while wait(0.1) and _G.delete do
+for i, v in pairs(game.Players.LocalPlayer.Pets:GetChildren()) do
+wait()
+if(tonumber(v.DiamondMultiplier.Value) <= tonumber(_G.deletedm)) and (tonumber(v.ClickMultiplier.Value) <= tonumber(_G.deletecm)) then
+local args = {
+[1] = v.Pet_ID.Value,
+[2] = true
+}
+game:GetService("ReplicatedStorage").RemoteEvents.PetDelete:FireServer(unpack(args))
+end
+end
+end
+end)
+end)
+Pets:Toggle("(OLD) Auto delete2", function(toggle)
+spawn(function()
+_G.delete = toggle
+while wait(0.1) and _G.delete do
+for i, v in pairs(game.Players.LocalPlayer.Pets:GetChildren()) do
+wait()
+if(tonumber(v.DiamondMultiplier.Value) <= tonumber(_G.deletedm)) or (tonumber(v.ClickMultiplier.Value) <= tonumber(_G.deletecm)) then
+local args = {
+[1] = v.Pet_ID.Value,
+[2] = true
+}
+game:GetService("ReplicatedStorage").RemoteEvents.PetDelete:FireServer(unpack(args))
+end
+end
+end
+end)
+--]]
 end)
 local stuff = w:CreateFolder("Stuff.")
 if(_G.ws == nil) then
